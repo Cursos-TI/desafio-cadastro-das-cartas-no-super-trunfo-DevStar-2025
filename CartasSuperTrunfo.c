d51d5f4a50bcea70128a0ae0ad0975b310f5676d
DevStar-2025 <marcus_tms@hotmail.com>
2025-08-03 15:28:12 +0000

    SuperTrunfoLogicaAventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
-int main(){
-    
-    printf("Desafio Super Trunfo\n");
-
-    //Declaração de variáveis 
-
+int main() {
+    // Declaração de variáveis
     char estado1[30], estado2[30];
     char nomecidade1[40], nomecidade2[40];
     char codigo1[4], codigo2[4];
@@ -15,9 +10,36 @@ int main(){
     int pontos_turisticos1, pontos_turisticos2;
     float densidade1, densidade2;
     float pibpercap1, pibpercap2;
-    float superpoder1 , superpoder2;
-    unsigned long int populacaototal;
-    char atributo[50];
+    float superpoder1, superpoder2;
+    int atributo;
+    char tecla;
+    int opcao;
+
+    // Menu Principal
+    while (opcao != 3) {
+        printf("\n### MENU PRINCIPAL! ###\n");
+        printf("1 - Iniciar o Jogo\n");
+        printf("2 - Regras do jogo\n");
+        printf("3 - Sair do jogo\n");
+        printf("Escolha uma opção!\n");
+        scanf("%d", &opcao);
+        getchar();
+
+        switch (opcao) {
+            case 1:
+            printf("Desafio Super Trunfo\n");
+            printf("\n### Escolha um atributo para comparação ###\n");
+    printf("1 - População\n");
+    printf("2 - Area\n");
+    printf("3 - Pontos Turisticos\n");
+    printf("4 - Densidade Populacional\n");
+    printf("5 - PIB\n");
+    printf("6 - Super Poder\n");
+    scanf("%d", &atributo);
+    getchar();
+
+
+    
 
     //Coletando os dados da primeira carta com o usuario
 
@@ -63,7 +85,7 @@ int main(){
     densidade2 = (float) (populacao2 / area2);
     pibpercap2 = (float) (pib2 / populacao2);
 
-    superpoder1 = (1/densidade2 + populacao2 + area2 + pib2 +pontos_turisticos2);
+    superpoder2 = (1/densidade2 + populacao2 + area2 + pib2 +pontos_turisticos2);
     printf("Super poder :%.2f\n",superpoder2 );
 
     //Exibição dos dados da primeira carta
@@ -91,35 +113,87 @@ int main(){
     printf("Pontos turísticos: %d\n", pontos_turisticos2);
     printf("Densidade populacional: %.2f\n" , densidade2);
     printf("PIB per Capta: %.2f\n", pibpercap2);
+    
 
-    /*Atributo para comparação de acordo com a soma das duas populações , se for impar
-será utilizado o PIB se for par será utilizado os pontos turisticos*/
-    populacaototal = populacao1 + populacao2;
-
-    // Definindo o atributo para comparação
-    if (populacaototal % 2 == 0) {
-        strcpy(atributo, "Pontos Turisticos");
-    } else {
-        strcpy(atributo, "PIB");
-    }
-
-    printf("\nO Atributo escolhido foi %s\n", atributo);
-
+  
     // Comparando as cartas com o atributo sorteado
+//Atributo POPULAÇÂO
     printf("\nComparação das cartas\n");
-    if (strcmp(atributo, "Pontos Turisticos") == 0) {
-        if (pontos_turisticos1 > pontos_turisticos2) {
-            printf("Parabens a carta %s Ganhou nos %s!\n", estado1, atributo);
-        } else {
-            printf("Parabens a carta %s Ganhou nos %s!\n", estado2, atributo);
-        }
-    } else {
-        if (pib1 > pib2) {
-            printf("Parabens a carta %s Ganhou no %s!\n", estado1, atributo);
-        } else {
-            printf("Parabens a carta %s ganhou no %s!\n", estado2, atributo);
-        }
+    if (atributo == 1 && populacao1 > populacao2) {
+    printf(" Parabens a carta %s ganhou em POPULAÇÃO\n", estado1);
+}   if (atributo == 1 && populacao1 < populacao2) {
+    printf(" Parabens a carta %s ganhou em POPULAÇÃO\n", estado2);
+}   if (atributo == 1 && populacao1 == populacao2) {
+    printf(" EMPATE em POPULAÇÃO\n");
+}
+
+// Atributo AREA
+
+    if (atributo == 2 && area1 > area2) {
+    printf(" Parabens a carta %s ganhou em AREA\n", estado1);
+}   if (atributo == 2 && area1 < area2) {
+    printf(" Parabens a carta %s ganhou em Area\n", estado2);
+}   if (atributo == 2 && area1 == area2) {
+    printf(" EMPATE em Area\n");
+    } 
+
+//Atributo PONTOS TURISTICOS
+
+    if (atributo == 3 && pontos_turisticos1 > pontos_turisticos2) {
+    printf(" Parabens a carta %s ganhou em PONTOS TURISTICOS\n", estado1);
+}   if (atributo == 3 && pontos_turisticos1 < pontos_turisticos2) {
+    printf(" Parabens a carta %s ganhou em PONTOS TURISTICOS\n", estado2);
+}   if (atributo == 3 && pontos_turisticos1 == pontos_turisticos2) {
+    printf(" EMPATE em PONTOS TURISTICOS\n");
+    } 
+
+//Atributo DENSIDADE POPULACIONAL
+    
+    if (atributo == 4 && densidade1 > densidade2) {
+    printf(" Parabens a carta %s ganhou em Densidade Populacional\n", estado1);
+}   if (atributo == 4 && densidade1 < densidade2) {
+    printf(" Parabens a carta %s ganhou em Densidade Populacional\n", estado2);
+}   if (atributo == 4 && densidade1 == densidade2) {
+    printf(" EMPATE em Densidade Populacional\n");
     }
 
-    return 0;
+//atributo PIB
+    
+    if (atributo == 5 && pib1 > pib2) {
+    printf(" Parabens a carta %s ganhou em PIB\n", estado1);
+}   if (atributo == 5 && pib1 < pib2) {
+    printf(" Parabens a carta %s ganhou em PIB\n", estado2);
+}   if (atributo == 5 && pib1 == pib2) {
+    printf(" EMPATE em PIB\n");
+    }
+
+//Atributo SUPER PODER
+    
+    if (atributo == 6 && superpoder1 > superpoder2) {
+    printf(" Parabens a carta %s ganhou em Super Poder\n", estado1);
+}   if (atributo == 6 && superpoder1 < superpoder2) {
+    printf(" Parabens a carta %s ganhou em Super Poder\n", estado2);
+}   if (atributo == 6 && superpoder1 == superpoder2) {
+    printf(" EMPATE em Super Poder\n");
+    }
+
+ break;
+            case 2:
+                printf(" A regra geral é: vence a carta com o maior valor no atributo escolhido\n");
+                printf(" Porém, para a Densidade Demográfica, a regra inverte\n");
+                printf(" Vence a carta com o menor valor\n");
+                printf("\n #para retornar ao menu principal aperte 9#\n");
+                char tecla;
+                do {
+                    tecla = getchar();
+                } while (tecla != '9');
+                break;
+            case 3:
+                printf("Saindo do jogo...\n");
+                return 0;
+            default:
+                printf("Opção incorreta , tente novamente!\n");
+                break;
+        }
+    }
 }
